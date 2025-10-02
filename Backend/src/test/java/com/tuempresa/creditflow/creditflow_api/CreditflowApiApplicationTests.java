35b5048cc348e19478ee87b7e408799eb9ebec7a
Delmer Teodomiro Rodriguez Gutierrez <jindrg@gmail.com>
2025-10-02 13:18:51 -0300

    init: proyecto base Spring Boot generado con Spring Initializr

---
+++ Backend/src/test/java/com/tuempresa/creditflow/creditflow_api/CreditflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuempresa.creditflow.creditflow_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditflowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
