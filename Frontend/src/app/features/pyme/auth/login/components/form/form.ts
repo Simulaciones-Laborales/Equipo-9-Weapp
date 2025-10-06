0c8d5b91078c3f7887f7b80811aa5201970bc579
Alben Bustamante <albenbustamante@gmail.com>
2025-10-06 15:30:03 -0500

    add: link to register page

--- Frontend/src/app/features/pyme/auth/login/components/form/form.ts
+++ Frontend/src/app/features/pyme/auth/login/components/form/form.ts
@@ -5,10 +5,18 @@ import { InputTextModule } from 'primeng/inputtext';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { PasswordModule } from 'primeng/password';
 import { ButtonModule } from 'primeng/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-form',
-  imports: [ReactiveFormsModule, InputTextModule, FloatLabelModule, PasswordModule, ButtonModule],
+  imports: [
+    ReactiveFormsModule,
+    InputTextModule,
+    FloatLabelModule,
+    PasswordModule,
+    ButtonModule,
+    RouterLink,
+  ],
   templateUrl: './form.html',
   styleUrl: './form.css',
 })
