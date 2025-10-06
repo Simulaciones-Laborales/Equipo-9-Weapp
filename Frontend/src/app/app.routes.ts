09b83d8dc1dd64e7e3bb66c5a23d573722b8e1cf
Alben Bustamante <albenbustamante@gmail.com>
2025-10-06 10:37:37 -0500

    define: pyme auth routes

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'pyme',
+    loadChildren: () => import('./features/pyme/pyme.routes'),
+  },
+];
