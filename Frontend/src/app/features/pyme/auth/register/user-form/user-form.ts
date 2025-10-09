1b52d1b387bc10a077046ee03fdaed5a3a43dae4
Alben Bustamante <albenbustamante@gmail.com>
2025-10-09 15:07:28 -0500

    update: users register form

--- Frontend/src/app/features/pyme/auth/register/user-form/user-form.ts
+++ Frontend/src/app/features/pyme/auth/register/user-form/user-form.ts
@@ -3,13 +3,12 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { emailRegex } from '@core/utils';
 import { FloatLabel } from 'primeng/floatlabel';
 import { InputText } from 'primeng/inputtext';
-import { Password } from 'primeng/password';
 import { Checkbox } from 'primeng/checkbox';
 import { Linker } from '../../components/linker/linker';
 
 @Component({
   selector: 'app-user-form',
-  imports: [ReactiveFormsModule, FloatLabel, InputText, Password, Checkbox, Linker],
+  imports: [ReactiveFormsModule, FloatLabel, InputText, Checkbox, Linker],
   templateUrl: './user-form.html',
   styleUrl: './user-form.css',
 })
@@ -17,9 +16,9 @@ export class UserForm {
   private readonly _fb = inject(FormBuilder);
 
   readonly form = this._fb.group({
-    name: ['', Validators.required],
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
     email: ['', [Validators.required, Validators.pattern(emailRegex)]],
-    password: ['', Validators.required],
-    repeatPassword: ['', Validators.required],
+    contact: ['', Validators.required],
   });
 }
