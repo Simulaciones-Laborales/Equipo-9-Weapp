19e2730307c33e3a8c7bda40139e56b83cb8b6a5
Alben Bustamante <albenbustamante@gmail.com>
2025-10-14 14:11:51 -0500

    design: credits header

--- Frontend/src/app/features/pyme/credits/credits.ts
+++ Frontend/src/app/features/pyme/credits/credits.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Title } from '@components/title/title';
+import { Card } from 'primeng/card';
 
 @Component({
   selector: 'app-credits',
-  imports: [],
+  imports: [Title, Card],
   templateUrl: './credits.html',
   styleUrl: './credits.css',
 })
