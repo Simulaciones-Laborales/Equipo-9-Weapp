bfd4123b0c15650794358cb99cf175ca28aaa3e3
Alben Bustamante <albenbustamante@gmail.com>
2025-10-14 16:31:33 -0500

    update: token interceptor to skip some endpoints; define: logic to get credits

--- Frontend/src/app/core/interceptors/token-interceptor.ts
+++ Frontend/src/app/core/interceptors/token-interceptor.ts
@@ -1,8 +1,16 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { TokenStorage } from '@core/services/token-storage';
+import { SKIP_INTERCEPTOR_HEADER } from '@core/utils/http-utils';
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
+  if (req.headers.has(SKIP_INTERCEPTOR_HEADER)) {
+    const newHeaders = req.headers.delete(SKIP_INTERCEPTOR_HEADER);
+    const newReq = req.clone({ headers: newHeaders });
+
+    return next(newReq);
+  }
+
   const tokenStorage = inject(TokenStorage);
   const token = tokenStorage.token();
 
