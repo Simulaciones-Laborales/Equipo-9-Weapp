832377ebeaeabecea8508d1d82e9512a96513b27
Alben Bustamante <albenbustamante@gmail.com>
2025-10-14 15:42:23 -0500

    add: basic table for credits history

--- Frontend/src/app/features/pyme/credits/credits.ts
+++ Frontend/src/app/features/pyme/credits/credits.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Title } from '@components/title/title';
 import { Card } from 'primeng/card';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-credits',
-  imports: [Title, Card],
+  imports: [Title, Card, TableModule],
   templateUrl: './credits.html',
   styleUrl: './credits.css',
 })
