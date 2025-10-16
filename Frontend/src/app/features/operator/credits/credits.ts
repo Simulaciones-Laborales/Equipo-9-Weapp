174ff3dc26d8dc400a2cefc606c7ab93c89011bb
Alben Bustamante <albenbustamante@gmail.com>
2025-10-16 12:38:00 -0500

    add: temporal table

--- Frontend/src/app/features/operator/credits/credits.ts
+++ Frontend/src/app/features/operator/credits/credits.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { Title } from '@components/title/title';
+import { Subtitle } from '@components/subtitle/subtitle';
+import { TableModule } from 'primeng/table';
+import { CurrencyPipe, DatePipe } from '@angular/common';
+import { Button } from 'primeng/button';
 
 @Component({
   selector: 'app-credits',
-  imports: [],
+  imports: [Title, Subtitle, TableModule, CurrencyPipe, DatePipe, Button],
   templateUrl: './credits.html',
   styleUrl: './credits.css',
 })
-export default class Credits {}
+export default class Credits {
+  today = new Date();
+}
