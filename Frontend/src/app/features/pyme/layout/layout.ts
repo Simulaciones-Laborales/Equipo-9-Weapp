62b1ef30c0c208eacff02ff00f7f13086f52806f
Alben Bustamante <albenbustamante@gmail.com>
2025-10-18 14:45:14 -0500

    remove: unused import

--- Frontend/src/app/features/pyme/layout/layout.ts
+++ Frontend/src/app/features/pyme/layout/layout.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
-import { Card } from 'primeng/card';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet, Navbar, Card],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
