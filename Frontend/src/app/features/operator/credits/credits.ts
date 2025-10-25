aceb1ea6ea313b7ad852cd83e73a5184dafb4bc8
Alben Bustamante <albenbustamante@gmail.com>
2025-10-25 12:14:10 -0500

    update: operator credits page

--- Frontend/src/app/features/operator/credits/credits.ts
+++ Frontend/src/app/features/operator/credits/credits.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
-import { Title } from '@components/title/title';
-import { Subtitle } from '@components/subtitle/subtitle';
 import { TableModule } from 'primeng/table';
 import { CurrencyPipe, DatePipe } from '@angular/common';
 import { Button } from 'primeng/button';
+import { Header } from '../components/header/header';
+import { Tab, TabList, TabPanel, Tabs, TabPanels } from 'primeng/tabs';
 
 @Component({
   selector: 'app-credits',
-  imports: [Title, Subtitle, TableModule, CurrencyPipe, DatePipe, Button],
+  imports: [
+    TableModule,
+    CurrencyPipe,
+    DatePipe,
+    Button,
+    Header,
+    Tabs,
+    TabList,
+    Tab,
+    TabPanel,
+    TabPanels,
+  ],
   templateUrl: './credits.html',
   styleUrl: './credits.css',
 })
