85189ac69febcee91db83c4e0e100faf21267209
Alben Bustamante <albenbustamante@gmail.com>
2025-10-26 15:27:30 -0500

    feat: new credit details page for operators

--- Frontend/src/app/features/operator/credits/components/credits-table/credits-table.ts
+++ Frontend/src/app/features/operator/credits/components/credits-table/credits-table.ts
@@ -8,6 +8,7 @@ import { PageableResponse } from '@core/types';
 import { CurrencyPipe, DatePipe } from '@angular/common';
 import { CreditApplicationPurposePipe } from 'app/pipes/credit-application-purpose-pipe';
 import { CreditApplicationStatusPipe } from 'app/pipes/credit-application-status-pipe';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-credits-table',
@@ -19,6 +20,7 @@ import { CreditApplicationStatusPipe } from 'app/pipes/credit-application-status
     DatePipe,
     CreditApplicationPurposePipe,
     CreditApplicationStatusPipe,
+    RouterLink,
   ],
   templateUrl: './credits-table.html',
   styleUrl: './credits-table.css',
@@ -27,4 +29,8 @@ export class CreditsTable {
   readonly onPageChange = output<PaginatorState>();
   readonly credits = input.required<PageableResponse<CreditApplicationResponse> | null>();
   readonly menu = input.required<Menu>();
+
+  route(id: string) {
+    return `./${id}`;
+  }
 }
