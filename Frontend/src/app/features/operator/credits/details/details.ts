c03662cf10cd188c4104f8d9600632c84c932d85
Alben Bustamante <albenbustamante@gmail.com>
2025-10-26 15:41:34 -0500

    add: breadcrumb to operator credits pages

--- Frontend/src/app/features/operator/credits/details/details.ts
+++ Frontend/src/app/features/operator/credits/details/details.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { Header } from '@features/operator/components/header/header';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-details',
-  imports: [],
+  imports: [Header],
   templateUrl: './details.html',
   styleUrl: './details.css',
 })
-export default class Details {}
+export default class Details {
+  readonly breadcrumbItems: MenuItem[] = [
+    { label: 'Solicitudes de crédito', icon: 'pi pi-home', routerLink: '../' },
+    { label: 'Detalles', routerLink: './' },
+  ];
+}
