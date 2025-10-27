29a18d32b4dfca56bf94dd2a8b1910e7a5af7136
Alben Bustamante <albenbustamante@gmail.com>
2025-10-27 15:30:47 -0500

    update: auth design

--- Frontend/src/app/features/auth/login/login.ts
+++ Frontend/src/app/features/auth/login/login.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Form } from './components/form/form';
 import { CardModule } from 'primeng/card';
+import { Logo } from '@components/logo/logo';
 
 @Component({
   selector: 'app-login',
-  imports: [Form, CardModule],
+  imports: [Form, CardModule, Logo],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
